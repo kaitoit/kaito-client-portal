13697c286bf30d3a4fee6b366e948ec9fa68972c
Robert Miller <rmiller@heat.com.au>
2025-05-29 13:13:21 +1000

    hellomate2869

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 // src/index.js
+import "./App.css";                 // ← load global styles
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
 import { PublicClientApplication } from "@azure/msal-browser";
 import { MsalProvider } from "@azure/msal-react";
 import { msalConfig } from "./authConfig";
+import App from "./App";
 
 const msalInstance = new PublicClientApplication(msalConfig);
 
