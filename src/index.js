52b5b385e0f843daad8fb33b97bd21aa7890eb91
Robert Miller <rmiller@heat.com.au>
2025-05-29 11:19:47 +1000

    fs324g

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
+// src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { PublicClientApplication } from "@azure/msal-browser";
+import { MsalProvider } from "@azure/msal-react";
 import { msalConfig } from "./authConfig";
 
 const msalInstance = new PublicClientApplication(msalConfig);
 
-msalInstance.handleRedirectPromise()
-  .then(() => {
-    ReactDOM.createRoot(document.getElementById("root")).render(
-      <React.StrictMode>
-        <App msalInstance={msalInstance} />
-      </React.StrictMode>
-    );
-  })
-  .catch((error) => {
-    console.error("MSAL redirect error:", error);
-  });
+msalInstance.handleRedirectPromise().then(() => {
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <MsalProvider instance={msalInstance}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </MsalProvider>
+    </React.StrictMode>
+  );
+});
