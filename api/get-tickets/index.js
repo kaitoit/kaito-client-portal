f26bc2296f80a39604178ba0ee0a90923e2a9cce
Robert Miller <rjmiller@heattreatmentaustralia.onmicrosoft.com>
2025-07-15 13:45:30 +1000

    yeahbuddynothingbutapeanut

--- api/get-tickets/index.js
+++ api/get-tickets/index.js
@@ -8,34 +8,24 @@ const databaseId = "SupportTickets";
 const containerId = "Tickets";
 
 module.exports = async function (context, req) {
-  // For demo, get email from query string
-  const userEmail = req.query.email;
-
-  if (!userEmail) {
-    context.res = { status: 400, body: "User email is required" };
-    return;
-  }
-
   try {
     const container = client.database(databaseId).container(containerId);
 
-    // Query tickets by email partition key
-    const querySpec = {
-      query: "SELECT * FROM c WHERE c.email = @userEmail",
-      parameters: [{ name: "@userEmail", value: userEmail }],
+    const query = {
+      query: "SELECT * FROM c ORDER BY c.submittedAt DESC"
     };
 
-    const { resources: tickets } = await container.items.query(querySpec).fetchAll();
+    const { resources: tickets } = await container.items.query(query).fetchAll();
 
     context.res = {
       status: 200,
-      body: tickets,
+      body: tickets
     };
   } catch (err) {
-    context.log.error("Error fetching tickets:", err.message);
+    context.log("Error fetching tickets:", err.message);
     context.res = {
       status: 500,
-      body: "Server error while fetching tickets",
+      body: "Server error while fetching tickets"
     };
   }
 };
